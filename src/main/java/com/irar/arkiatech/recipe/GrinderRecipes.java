418bfc494d37d256ee2c1e06247a6430e93340d8
IrresoluteArkia <jacob@boytroop.com>
2019-01-25 17:03:35 -0500

    Add grinder recipes for ores from mods using ore dict

--- src/main/java/com/irar/arkiatech/recipe/GrinderRecipes.java
+++ src/main/java/com/irar/arkiatech/recipe/GrinderRecipes.java
@@ -7,6 +7,9 @@ import com.irar.arkiatech.handlers.ATItems;
 
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.oredict.OreDictionary;
+import scala.actors.threadpool.Arrays;
 
 public class GrinderRecipes {
 
@@ -39,6 +42,76 @@ public class GrinderRecipes {
 		allRecipes.add(new GrinderRecipe(new ItemStack(Items.DIAMOND_LEGGINGS), new ItemStack(ATItems.zeDiamond, 6)));
 		allRecipes.add(new GrinderRecipe(new ItemStack(Items.DIAMOND_CHESTPLATE), new ItemStack(ATItems.zeDiamond, 7)));
 		allRecipes.add(new GrinderRecipe(new ItemStack(Items.DIAMOND_HELMET), new ItemStack(ATItems.zeDiamond, 4)));
+		
+		addOreToDustRecipes();
+	}
+
+	private static void addOreToDustRecipes() {
+		String[] ores = OreDictionary.getOreNames();
+		String[] oreOres = getStringsStartingWith(ores, "ore");
+		String[] dustOres = getStringsStartingWith(ores, "dust");
+		subtractBeginningChars(oreOres, 3);
+		subtractBeginningChars(dustOres, 4);
+		String[] matching = getMatching(oreOres, dustOres);
+		
+		for(String ore : matching) {
+			if(!isIngredientFor("ore" + ore)) {
+				NonNullList<ItemStack> outputs = OreDictionary.getOres("dust" + ore);
+				if(outputs.size() > 0) {
+					ItemStack output = outputs.get(0).copy();
+					output.setCount(2);
+					allRecipes.add(new GrinderRecipe("ore" + ore, output));
+				}
+			}
+		}
+	}
+
+	private static boolean isIngredientFor(String ore) {
+		NonNullList<ItemStack> stacks = OreDictionary.getOres(ore);
+		for(ItemStack stack : stacks) {
+			for(GrinderRecipe recipe : allRecipes) {
+				if(recipe.matches(stack)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static String[] getMatching(String[] s1, String[] s2) {
+		List<String> s3 = new ArrayList<>();
+		List<String> s2a = Arrays.asList(s2);
+		for(String s : s1) {
+			if(s.length() == 0) {
+				// Pointless to add empty
+				continue;
+			}
+			if(s2a.contains(s)) {
+				s3.add(s);
+			}
+		}
+		return s3.toArray(new String[0]);
+	}
+
+	private static void subtractBeginningChars(String[] ss, int num) {
+		for(int i = 0; i < ss.length; i++) {
+			String s = ss[i];
+			if(s.length() <= num) {
+				ss[i] = "";
+			}else {
+				ss[i] = s.substring(num);
+			}
+		}
+	}
+
+	private static String[] getStringsStartingWith(String[] ss, String string) {
+		List<String> starting = new ArrayList<>();
+		for(String s : ss) {
+			if(s.startsWith(string)) {
+				starting.add(s);
+			}
+		}
+		return starting.toArray(new String[0]);
 	}
 	
 }
