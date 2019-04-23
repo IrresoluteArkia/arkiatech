597a8a82a3576d2c7e246b335ca014a3cf4aec1a
IrresoluteArkia <jacob@boytroop.com>
2019-04-22 21:41:08 -0400

    Add shift-click functionality to inventories

--- src/main/java/com/irar/arkiatech/gui/container/ContainerBase.java
+++ src/main/java/com/irar/arkiatech/gui/container/ContainerBase.java
@@ -3,6 +3,7 @@ package com.irar.arkiatech.gui.container;
 import com.irar.arkiatech.gui.container.slot.SlotDriftInput;
 import com.irar.arkiatech.gui.container.slot.SlotGrinderOutput;
 import com.irar.arkiatech.gui.container.slot.UpgradeSlot;
+import com.irar.arkiatech.item.ATMachineUpgrade;
 import com.irar.arkiatech.tileentity.TileGenerator;
 import com.irar.arkiatech.tileentity.TileMachineBase;
 import com.irar.arkiatech.tileentity.UpgradeInventory;
@@ -13,6 +14,7 @@ import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IContainerListener;
 import net.minecraft.inventory.Slot;
 import net.minecraft.inventory.SlotFurnaceFuel;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -26,6 +28,47 @@ public class ContainerBase<T extends TileMachineBase> extends Container {
 		addUpgradeInv(te.upgrades, getUpgradeLocation());
 	}
 	
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
+		ItemStack stack = ItemStack.EMPTY;
+		Slot slot = this.inventorySlots.get(index);
+		
+        if (slot != null && slot.getHasStack())
+        {
+            ItemStack itemstack1 = slot.getStack();
+            stack = itemstack1.copy();
+            if(stack.getItem() instanceof ATMachineUpgrade) {
+            	if(index < 36) {
+                	if(!this.mergeItemStack(itemstack1, 36, 40, false)) {
+                		return ItemStack.EMPTY;
+                	}
+				} /*
+					 * else if(index >= 36 && index < 40) { if(!this.mergeItemStack(itemstack1, 0,
+					 * 36, false)) { return ItemStack.EMPTY; } }
+					 */
+            }else 
+            if(index < 36) {
+            	if(!this.mergeItemStack(itemstack1, 36, this.inventorySlots.size(), false)) {
+            		return ItemStack.EMPTY;
+            	}
+            }else {
+            	if(!this.mergeItemStack(itemstack1, 0, 36, false)) {
+            		return ItemStack.EMPTY;
+            	}
+            }
+
+            if (itemstack1.isEmpty())
+            {
+                slot.putStack(ItemStack.EMPTY);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+        }
+        return stack;
+	}
+
 	/**
 	 * @return two ints which determine the top left corner of the upgrade inventory
 	 */
